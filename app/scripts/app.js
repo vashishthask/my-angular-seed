a4574b89bf56572356dc171404271bea9d79df29
Andrew C. Greenberg <wizardwerdna@gmail.com>
2013-10-04 12:34:12 -0700

    initial commit -- raw 'yo angular'

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('myAngularSeedApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
