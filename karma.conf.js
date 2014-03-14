2834c7aa4881d112cbdaebd5e9e04634860525db
Andrew C. Greenberg <wizardwerdna@gmail.com>
2014-03-14 00:00:20 -0700

    update minimalist application mode

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
-
+'use strict';
 module.exports = function(config) {
   config.set({
     // base path, that will be used to resolve files and exclude
@@ -18,9 +18,12 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-test-scaffold/*.js',
+      '*.js',
       'scripts/*.js',
       'scripts/**/*.js',
+      '*.html',
       'views/*.html',
+      '../test/*.js',
       '../test/mock/**/*.js',
       '../test/spec/**/*.js'
     ],
